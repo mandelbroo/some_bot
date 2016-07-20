33942e087a9f33db28afd7b2aec5831ded668e51
vitaliy <vitaliy@globalit.no>
2016-07-21 00:30:07 +0300

    add /help for the list of commands

--- app/counsciousness.rb
+++ app/counsciousness.rb
@@ -1,8 +1,15 @@
+COMMANDS = [
+  '/sup - say hi to the bot',
+  '/help - obviously'
+]
+
 class Counsciousness
   def read(message)
     case message.text
     when '/sup'
-      "Sup, #{message.from.first_name}!"
+      "Sup #{message.from.first_name}!"
+    when '/help'
+      COMMANDS.join("\n")
     end
   end
 end
